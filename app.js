48fe1587d9c25c3515f658d5229638dd28a314e7
Hamid64Raza <hamidraza.raza@gamil.com>
2025-12-07 22:29:23 +0530

    modules in typeScript

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var student_1 = require("./student");
+var s = new student_1.Student("Hamid");
+console.log(s.name);
---
+++ app.ts
@@ -0,0 +1,4 @@
+import { Student } from "./student";
+
+let s = new Student("Hamid");
+console.log(s.name);
